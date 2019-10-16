0e5bd2fcb7da1cb0d9d933c0d28cc4c8003b0e99
GavasBadger <56650394+GavasBadger@users.noreply.github.com>
2019-10-17 01:49:06 +0530

    Add files via upload

---
+++ forRange.py
@@ -0,0 +1,5 @@
+print('for range funbction')
+
+for i in range(0, 10, 2):  #start, end, increment by 
+    print(str(i))
+# for taking desending order alternate the values
---
+++ hello.py
@@ -0,0 +1,10 @@
+# Print hello world and ask for your name
+
+print('Hello World!')
+print('What is your nraame ?')
+myName = input()    # For takin input as string
+print('Hello your name is '+myName)
+print(len(myName))
+print('whst is your age ?')
+myAge = input()
+print('Your age is '+str(int(myAge) + 1) + ' in next year')
