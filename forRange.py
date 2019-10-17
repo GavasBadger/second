d66c6f954df32155f11308019d1590de7750d6f8
Muzasarali <48006670+Muzasarali@users.noreply.github.com>
2019-10-17 14:59:33 +0530

    Update forRange.py

--- forRange.py
+++ forRange.py
@@ -1,5 +1,8 @@
 print('for range funbction')
-
-for i in range(0, 10, 2):  #start, end, increment by 
-    print(str(i))
+def fun(n):
+    for i in range(0,n, 2):  #start, end, increment by 
+        print(str(i))
+        
 # for taking desending order alternate the values
+n = input()
+fun(n)
